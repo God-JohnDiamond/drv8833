b1921bd8b1920193101f866e32a571e1164d85ac
John <1334769096@qq.com>
2020-12-23 16:08:15 +0800

    io mean drivers step motor

--- pwm/pwm.c
+++ pwm/pwm.c
@@ -1,3 +1,10 @@
+/*
+ * @Author: John Diamond
+ * @Date: 2020-12-23 11:51:03
+ * @LastEditors: John Diamond
+ * @LastEditTime: 2020-12-23 16:07:17
+ * @FilePath: /code/pwm/pwm.c
+ */
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -5,78 +12,25 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include "pwm.h"
 
-#define PWM_IOCTL_MAGIC				'p'
-#define ENABLE_PWM					_IOWR(PWM_IOCTL_MAGIC, 0, unsigned int)
-#define DISABLE_PWM					_IOWR(PWM_IOCTL_MAGIC, 1, unsigned int)
-
-#define SET_PWM_DUTY_CYCLE			_IOWR(PWM_IOCTL_MAGIC, 2, unsigned int)
-#define GET_PWM_DUTY_CYCLE			_IOWR(PWM_IOCTL_MAGIC, 3, unsigned int)
-#define SET_PWM_DUTY_CYCLE_PERCENT	_IOWR(PWM_IOCTL_MAGIC, 4, unsigned int)
-#define SET_PWM_ENABLE				_IOWR(PWM_IOCTL_MAGIC, 5, unsigned int)
-#define ENABLE_MUL_PWM				_IOWR(PWM_IOCTL_MAGIC, 6, unsigned int)
-#define ENABLE_FINSHALL_INTR		_IOWR(PWM_IOCTL_MAGIC, 7, unsigned int)
-#define ENABLE_FINSHONCE_INTR		_IOWR(PWM_IOCTL_MAGIC, 8, unsigned int)
-#define DISABLE_FINSHALL_INTR		_IOWR(PWM_IOCTL_MAGIC, 9, unsigned int)
-#define DISABLE_FINSHONCE_INTR		_IOWR(PWM_IOCTL_MAGIC, 10, unsigned int)
-#define WAIT_PWM_FINSHALL			_IOWR(PWM_IOCTL_MAGIC, 12, unsigned int)
-
-struct fh_pwm_config
+void forward(int fd)
 {
-	unsigned int period_ns;
-	unsigned int duty_ns;
-	unsigned int pulses;
-#define FH_PWM_STOPLVL_LOW		(0x0)
-#define FH_PWM_STOPLVL_HIGH		(0x3)
-#define FH_PWM_STOPLVL_KEEP		(0x1)
 
-#define FH_PWM_STOPCTRL_ATONCE		(0x10)
-#define FH_PWM_STOPCTRL_AFTERFINISH	(0x00)
-	unsigned int stop;
-	unsigned int delay_ns;
-	unsigned int phase_ns;
-	unsigned int percent;
-	unsigned int finish_once;
-	unsigned int finish_all;
-};
+}
 
-struct fh_pwm_status
+void reverse(int fd)
 {
-	unsigned int done_cnt;
-	unsigned int total_cnt;
-	unsigned int busy;
-	unsigned int error;
-};
-
-typedef struct { } spinlock_t;
-
-struct list_head {
-	struct list_head *next, *prev;
-};
-
-struct __wait_queue_head {
-	spinlock_t lock;
-	struct list_head task_list;
-};
-typedef struct __wait_queue_head wait_queue_head_t;
-
-struct fh_pwm_chip_data
-{
-	int id;
-	struct fh_pwm_config config;
-	struct fh_pwm_status status;
-	wait_queue_head_t wait_done;
-	void (*finishall_callback)(struct fh_pwm_chip_data *data);
-	void (*finishonce_callback)(struct fh_pwm_chip_data *data);
-};
+	
+}
 
 
 int main(int argc, char *argv[])
 {
 	int pwm_fd;
+	int j;
 	struct fh_pwm_chip_data chip_data = {0};
 	int i;
-	
 
 	pwm_fd = open("/dev/fh_pwm", O_RDWR | O_SYNC);
 	if(pwm_fd == -1)
@@ -84,53 +38,51 @@ int main(int argc, char *argv[])
 		printf("open failed\n");
 		return -1;
 	}
-	while(1)
-	{
-		for(i = 0; i < 8; i++){
-		chip_data.id = i;
-		if(-1 == ioctl(pwm_fd, GET_PWM_DUTY_CYCLE, &chip_data))
-		{
-			printf("ioctl0 failed\n");
-			return -1;
-		}
-		chip_data.config.period_ns = 20000;
-		chip_data.config.duty_ns = 10000;
-		chip_data.config.pulses = 5;
-		chip_data.config.stop = FH_PWM_STOPLVL_HIGH;
-		//chip_data.config.delay_ns = 100;
-		//chip_data.config.phase_ns = 100;
-		//chip_data.config.percent = 50;
 
-		if(-1 == ioctl(pwm_fd, SET_PWM_DUTY_CYCLE, &chip_data))
-		{
-			printf("ioctl1 failed\n");
-			return -1;		
-		}
+	for(j = 0; j < 100; j++){
+		for(i = 0; i < 4; i++){
+			switch(i){
+				case 0: chip_data.id = 2; break;
+				case 1: chip_data.id = 1; break;
+				case 2: chip_data.id = 3; break;
+				case 3: chip_data.id = 0; break;
+			}
+			if(-1 == ioctl(pwm_fd, GET_PWM_DUTY_CYCLE, &chip_data))
+			{
+				printf("ioctl0 failed\n");
+			}
+			chip_data.config.period_ns = 20000;		// 20us
+			chip_data.config.duty_ns = 19999;		// duty 100%
+			chip_data.config.pulses = 500;			// 500 times * 20us = 10ms
+			chip_data.config.stop = FH_PWM_STOPLVL_LOW;
+			chip_data.config.delay_ns = 0;
 
-		if(-1 == ioctl(pwm_fd, ENABLE_MUL_PWM, 0x0))
-		{
-			printf("ioctl1 failed\n");
-			return -1;		
-		}
-	
-		if(-1 == ioctl(pwm_fd, ENABLE_PWM, &chip_data))
-		{
-			printf("ioctl2 failed\n");
-			return -1;
-		}
-		usleep(200);
+			if(-1 == ioctl(pwm_fd, SET_PWM_DUTY_CYCLE, &chip_data))
+			{
+				printf("ioctl1 failed\n");	
+			}
 
-		if(-1 == ioctl(pwm_fd, DISABLE_PWM, &chip_data))
-		{
-			printf("ioctl3 failed\n");
-			return -1;
+			if(-1 == ioctl(pwm_fd, ENABLE_MUL_PWM, 0xff))
+			{
+				printf("ioctl1 failed\n");
+			}
+
+			if(-1 == ioctl(pwm_fd, ENABLE_PWM, &chip_data))
+			{
+				printf("ioctl2 failed\n");
+			}
+			usleep(1000);
+			//while(chip_data.status.done_cnt != chip_data.config.pulses);
+
+			if(-1 == ioctl(pwm_fd, DISABLE_PWM, &chip_data))
+			{
+				printf("ioctl3 failed\n");
+			}
+			printf("id = %d  done_cnt=%d   total_cnt=%d\n", chip_data.id, chip_data.status.done_cnt, chip_data.status.total_cnt);
 		}
-		usleep(200);
-		printf("id = %d  done_cnt=%d   total_cnt=%d   busy=%d \n", chip_data.id, chip_data.status.done_cnt, chip_data.status.total_cnt, chip_data.status.busy);
-		}
-		sleep(2);
 	}
-	
+
+	//sleep(2);
 
 	close(pwm_fd);
 
---
+++ pwm/pwm.h
@@ -0,0 +1,76 @@
+/*
+ * @Author: John Diamond
+ * @Date: 2020-12-23 14:23:17
+ * @LastEditors: John Diamond
+ * @LastEditTime: 2020-12-23 14:24:38
+ * @FilePath: /code/pwm/pwm.h
+ */
+#ifndef PWM_H_
+#define PWM_H_
+
+#define PWM_IOCTL_MAGIC				'p'
+#define ENABLE_PWM					_IOWR(PWM_IOCTL_MAGIC, 0, unsigned int)
+#define DISABLE_PWM					_IOWR(PWM_IOCTL_MAGIC, 1, unsigned int)
+
+#define SET_PWM_DUTY_CYCLE			_IOWR(PWM_IOCTL_MAGIC, 2, unsigned int)
+#define GET_PWM_DUTY_CYCLE			_IOWR(PWM_IOCTL_MAGIC, 3, unsigned int)
+#define SET_PWM_DUTY_CYCLE_PERCENT	_IOWR(PWM_IOCTL_MAGIC, 4, unsigned int)
+#define SET_PWM_ENABLE				_IOWR(PWM_IOCTL_MAGIC, 5, unsigned int)
+#define ENABLE_MUL_PWM				_IOWR(PWM_IOCTL_MAGIC, 6, unsigned int)
+#define ENABLE_FINSHALL_INTR		_IOWR(PWM_IOCTL_MAGIC, 7, unsigned int)
+#define ENABLE_FINSHONCE_INTR		_IOWR(PWM_IOCTL_MAGIC, 8, unsigned int)
+#define DISABLE_FINSHALL_INTR		_IOWR(PWM_IOCTL_MAGIC, 9, unsigned int)
+#define DISABLE_FINSHONCE_INTR		_IOWR(PWM_IOCTL_MAGIC, 10, unsigned int)
+#define WAIT_PWM_FINSHALL			_IOWR(PWM_IOCTL_MAGIC, 12, unsigned int)
+
+struct fh_pwm_config
+{
+	unsigned int period_ns;
+	unsigned int duty_ns;
+	unsigned int pulses;
+#define FH_PWM_STOPLVL_LOW		(0x0)
+#define FH_PWM_STOPLVL_HIGH		(0x3)
+#define FH_PWM_STOPLVL_KEEP		(0x1)
+
+#define FH_PWM_STOPCTRL_ATONCE		(0x10)
+#define FH_PWM_STOPCTRL_AFTERFINISH	(0x00)
+	unsigned int stop;
+	unsigned int delay_ns;
+	unsigned int phase_ns;
+	unsigned int percent;
+	unsigned int finish_once;
+	unsigned int finish_all;
+};
+
+struct fh_pwm_status
+{
+	unsigned int done_cnt;
+	unsigned int total_cnt;
+	unsigned int busy;
+	unsigned int error;
+};
+
+typedef struct { } spinlock_t;
+
+struct list_head {
+	struct list_head *next, *prev;
+};
+
+struct __wait_queue_head {
+	spinlock_t lock;
+	struct list_head task_list;
+};
+typedef struct __wait_queue_head wait_queue_head_t;
+
+struct fh_pwm_chip_data
+{
+	int id;
+	struct fh_pwm_config config;
+	struct fh_pwm_status status;
+	wait_queue_head_t wait_done;
+	void (*finishall_callback)(struct fh_pwm_chip_data *data);
+	void (*finishonce_callback)(struct fh_pwm_chip_data *data);
+};
+
+
+#endif
